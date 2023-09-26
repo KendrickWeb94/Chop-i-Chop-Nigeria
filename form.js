c411e4c59598efba6316987ed392d017ec6c83bf
Kendrick Victor <140568733+KendrickWeb94@users.noreply.github.com>
2023-09-26 00:04:47 +0000

    Add files via upload

---
+++ form.js
@@ -0,0 +1,17 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  grabCursor:true,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
